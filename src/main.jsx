9e45962be14e5e4f2e6c11aa1cceb4370e553cb1
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-17 11:11:14 +0600

    React-router-dom

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
