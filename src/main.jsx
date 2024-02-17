62345fc6a7bbef60724c0a56cf451dfedc943b2a
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-17 11:40:19 +0600

    Loading Data URL segments, layouts,

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./routers/root";
+import ErrorPage from "./error-page";
+import Contact from "./routers/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
